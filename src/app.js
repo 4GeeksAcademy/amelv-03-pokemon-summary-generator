70c9001b005ffb23c51bfcb014cd7fdf0a59fabd
amelv-03 <aiden.melv03@gmail.com>
2024-08-27 16:52:53 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -9,19 +9,90 @@ window.onload = function() {
   //write your code here
   document.querySelector("#default").addEventListener("click", function() {
     generateSummary("default");
-  }
-}
-const generateSummary = {
-  let pokemonName = ["Pikachu", "Rayquaza", "Magikarp", "Glaceon", "Palafin", "Tepig", "Shinx", "Lopunny", "Gardevoir", "Kingambit", "Staraptor", "Lucario", "Dragapult", "Garchomp", "Eevee", "Golisopod"];
-  let catchLocation = ["Route 1", "Victory Road", "Driftveil City", "the Pokemon Center", "Mount Lanakila", "Route 202", "the Pokemon Mansion", "Lake Verity"];
-  let nature = ["Hardy", "Lonely", "Adamant", "Naughty", "Brave", "Bold", "Docile", "Impish", "Lax", "Relaxed", "Modest", "Mild", "Bashful", "Rash", "Quiet", "Calm", "Careful", "Gentle", "Quirky", "Sassy", "Timid", "Hasty", "Naive", "Serious", "Jolly"]
-  let characteristic = ["Loves to eat","Proud of its power", "Sturdy body","Likes to run","Highly curious","Strong willed","Likes to relax","Impetuous and silly","Thoroughly cunning","Mischievous"]
-}
+  });
+};
+const generateSummary = () => {
+  let pokemonName = [
+    "Pikachu",
+    "Rayquaza",
+    "Magikarp",
+    "Glaceon",
+    "Palafin",
+    "Tepig",
+    "Shinx",
+    "Lopunny",
+    "Gardevoir",
+    "Kingambit",
+    "Staraptor",
+    "Lucario",
+    "Dragapult",
+    "Garchomp",
+    "Eevee",
+    "Golisopod"
+  ];
+  let catchLocation = [
+    "Route 1",
+    "Victory Road",
+    "Driftveil City",
+    "the Pokemon Center",
+    "Mount Lanakila",
+    "Route 202",
+    "the Pokemon Mansion",
+    "Lake Verity"
+  ];
+  let nature = [
+    "Hardy",
+    "Lonely",
+    "Adamant",
+    "Naughty",
+    "Brave",
+    "Bold",
+    "Docile",
+    "Impish",
+    "Lax",
+    "Relaxed",
+    "Modest",
+    "Mild",
+    "Bashful",
+    "Rash",
+    "Quiet",
+    "Calm",
+    "Careful",
+    "Gentle",
+    "Quirky",
+    "Sassy",
+    "Timid",
+    "Hasty",
+    "Naive",
+    "Serious",
+    "Jolly"
+  ];
+  let characteristic = [
+    "Loves to eat",
+    "Proud of its power",
+    "Sturdy body",
+    "Likes to run",
+    "Highly curious",
+    "Strong willed",
+    "Likes to relax",
+    "Impetuous and silly",
+    "Thoroughly cunning",
+    "Mischievous"
+  ];
 
-let pokemonNameIndex = Math.floor(Math.random() * pokemonName.length);
-let catchLocationIndex = Math.floor(Math.random() * catchLocation.length);
-let natureIndex = Math.floor(Math.random() * nature.length);
-let characteristicIndex = Math.floor(Math.random() * characteristic.length);
+  let pokemonNameIndex = Math.floor(Math.random() * pokemonName.length);
+  let catchLocationIndex = Math.floor(Math.random() * catchLocation.length);
+  let natureIndex = Math.floor(Math.random() * nature.length);
+  let characteristicIndex = Math.floor(Math.random() * characteristic.length);
 
-let fullSummary = pokemonName[pokemonNameIndex] + " was caught at" + catchLocation[catchLocationIndex] + "." + nature[natureIndex] + " nature." + characteristic[characteristicIndex] + ".";
-document.querySelector(".pokemonSummary").innerHTML = fullSummary;
+  let fullSummary =
+    pokemonName[pokemonNameIndex] +
+    " was caught at " +
+    catchLocation[catchLocationIndex] +
+    ". " +
+    nature[natureIndex] +
+    " nature. " +
+    characteristic[characteristicIndex] +
+    ".";
+  document.querySelector(".pokemonSummary").innerHTML = fullSummary;
+};
