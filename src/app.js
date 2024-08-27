10b92f4c0226d2a8eb6b2c93dc137dbca413057b
amelv-03 <aiden.melv03@gmail.com>
2024-08-27 00:53:03 +0000

    add natures and characteristics

--- src/app.js
+++ src/app.js
@@ -7,5 +7,11 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#default").addEventListener("click", function() {
+    generateSummary("default");
 };
+const generateSummary = {
+  let pokemonName = ["Pikachu", "Rayquaza", "Magikarp", "Glaceon", "Palafin", "Tepig", "Shinx", "Lopunny", "Gardevoir", "Kingambit"];
+  let catchLocation = ["Route 1", "Victory Road", "Driftveil City", "the Pokemon Center", "Mount Lanakila", "Route 202", "the Pokemon Mansion", "Lake Verity"];
+  let pokemonNature = []
+}
